b78cf025c5fc21d6b1b166e2fdfa6bd9494aae22
Omar Ocampo <49409578+omarocmpo7@users.noreply.github.com>
2024-04-26 14:36:01 -0700

    MenuSettings
    Las opciones de volumen y graficos ya funcionan

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject PantallaDeCarga;
     public Slider slider;
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
 
     public void Play_Button(int NumeroEscena)
     {

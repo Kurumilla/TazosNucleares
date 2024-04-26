5b002eca4c679ac5fead344dc45714897427115f
Omar Ocampo <49409578+omarocmpo7@users.noreply.github.com>
2024-04-26 00:20:26 -0700

    Menu Settings
    Esta listo el menu de opciones solo falta darle funcion a sus opciones y ya quedo lista la pantalla de carga al iniciar partida

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public void Play_Button()
+    public GameObject PantallaDeCarga;
+    public Slider slider;
+
+    public void Play_Button(int NumeroEscena)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(CargarAsync(NumeroEscena));
+    }
+
+    IEnumerator CargarAsync(int NumeroEscena)
+    {
+        AsyncOperation Operacion = SceneManager.LoadSceneAsync(NumeroEscena);
+
+        PantallaDeCarga.SetActive(true);
+
+        while(!Operacion.isDone)
+        {
+            float Progreso = Mathf.Clamp01(Operacion.progress / .9f);
+            slider.value = Progreso;
+
+            yield return null;
+        }
     }
 
     public void Exit_Button()

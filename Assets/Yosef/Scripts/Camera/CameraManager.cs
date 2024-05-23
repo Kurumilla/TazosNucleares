0a5a340303f55c0af88e4b81c4e9f887e83c3404
AlanFlores02 <yosefalanfloreslopez@outlook.com>
2024-05-23 09:20:47 -0700

    Focus+

---
+++ Assets/Yosef/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Mode
+{
+    Pivot,      //Rotate around the playground
+    Swivel,     //Tilt the camera around
+    Focus,      //Lerp position and rotation
+    Unfocus     //Return from focus
+}
+
+public class CameraManager : MonoBehaviour
+{
+    public Mode _mode;
+    private Mode mode;
+    Pivot pivot;
+    Swivel swivel;
+    Focus focus;
+
+    bool isDone;
+    public Transform target;
+    
+    void Start()
+    {
+        pivot = GetComponent<Pivot>();
+        swivel = GetComponentInChildren<Swivel>();
+        focus = GetComponent<Focus>();
+    }
+
+    void Update()
+    {
+        ChangeMode();
+        switch (mode)
+        {
+            case Mode.Pivot:
+                pivot.CameraMovement();
+                break;
+            case Mode.Swivel:
+                swivel.CameraMovement();
+                break;
+            case Mode.Focus:
+                isDone = focus.CameraMovement(1);
+                break;
+            case Mode.Unfocus:
+                isDone = focus.CameraMovement(-1);
+                break;
+        }
+    }
+
+    private void ChangeMode()
+    {
+        if (mode != Mode.Focus && _mode == Mode.Focus)     //If entering focus, set variables
+            SetFocus();
+        else if (mode == Mode.Focus && _mode != Mode.Focus)     //If exit from focus, go to unfocus instead
+            mode = Mode.Unfocus;
+        else if (mode == Mode.Unfocus && isDone)                //Wait for unfocus to be done
+            mode = _mode;
+        else if (mode != Mode.Unfocus)
+            mode = _mode;
+    }
+
+    public void SetFocus()
+    {
+        focus.SetVariables(target);
+        mode = Mode.Focus;
+    }
+}

9091966f4fbca1000e2621091c73bfab353e7639
AlanFlores02 <yosefalanfloreslopez@outlook.com>
2024-07-02 08:03:01 -0700

    Pointer (Railroading) 2.0
    Ya hace las cosas que faltaban.

--- Assets/Yosef/Scripts/RailroadingPointer.cs
+++ Assets/Yosef/Scripts/RailroadingPointer.cs
@@ -7,31 +7,38 @@ public class RailroadingPointer : MonoBehaviour
 {
     public float distanceToHide = 10f;
 
-    // Object References
+    [Header("Object References")]
     private Transform player;
-    private List<Vector3> enemiesLeft = new List<Vector3>();
+    public List<Transform> enemies = new List<Transform>();
+    public List<Sprite> sprites = new List<Sprite>();
 
-    // UI References
-    public RectTransform pointerPos, pointerRot;
+    [Header("UI References")]
+    public RectTransform pointerPos;
+    public RectTransform pointerRot;
     private RectTransform canvas;
+    public Image img;
+    private CanvasGroup visible;
 
     void Start()
     {
         // Resize
         canvas = FindObjectOfType<Canvas>().GetComponent<RectTransform>();
+        visible = GetComponent<CanvasGroup>();
         pointerPos.sizeDelta = new Vector2(canvas.rect.height / 4f, canvas.rect.height / 4f);
 
         // Add to the list the enemies you havent beat yet
         player = GameObject.FindWithTag("Player").transform;
-        GameObject[] all = GameObject.FindGameObjectsWithTag("Enemy");
-        foreach (GameObject enemy in all)
+        for (int i=enemies.Count-1; i>=0; i--)
         {
-            if (enemy.GetComponent<NPC_Dialogue>().status != NPC_Status.PlayerWins)
-                enemiesLeft.Add(enemy.transform.position);
+            if (enemies[i].gameObject.GetComponent<NPC_Dialogue>().status == NPC_Status.PlayerWins)
+            {
+                enemies.RemoveAt(i);
+                sprites.RemoveAt(i);
+            }
         }
 
         // Destroy if no enemies left
-        if (enemiesLeft.Count == 0)
+        if (enemies.Count == 0)
             Destroy(gameObject);
     }
 
@@ -39,14 +46,21 @@ public class RailroadingPointer : MonoBehaviour
     {
         // Point to the closest enemy in the list
         int id = 0;
-        for (int i=1; i<enemiesLeft.Count; i++)
+        for (int i=1; i<enemies.Count; i++)
         {
-            if (Vector3.Distance(player.position, enemiesLeft[i]) < Vector3.Distance(player.position, enemiesLeft[id]))
+            if (Vector3.Distance(player.position, enemies[i].position) < Vector3.Distance(player.position, enemies[id].position))
                 id = i;
         }
-        Vector3 dir = enemiesLeft[id] - player.position;
+        // Hide if close enough to see the enemy
+        if (Vector3.Distance(player.position, enemies[id].position) < distanceToHide)
+            visible.alpha = 0;
+        else
+            visible.alpha = 1;
 
-        // Set position
+        // Get position and image to use
+        Vector3 dir = enemies[id].position - player.position;
+        img.sprite = sprites[id];
+        // Calculate canvas position
         Vector2 pos = new Vector2(dir.x, dir.z).normalized * 1.8f * new Vector2(canvas.rect.height / 2f, canvas.rect.height / 2f);
         float ceiling = Mathf.Abs(pos.y) / (0.75f * canvas.rect.height / 2f);
         if (ceiling > 1)
